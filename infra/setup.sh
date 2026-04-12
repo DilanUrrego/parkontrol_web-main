48cae897090757c7709a08f968d5edbfd2f5e207
OnemaYepes <jumayemo108@gmail.com>
2026-04-11 22:40:00 -0500

    De nuevo el setup

--- infra/setup.sh
+++ infra/setup.sh
@@ -7,25 +7,10 @@ docker network create devops-net 2>/dev/null || echo "Red ya existe, continuando
   
 echo "==> Levantando Jenkins..."  
 # CRÍTICO: No dejes espacios después de la barra \
-docker run -d \
-  --name jenkins \
-  --network devops-net \
-  --restart always \
-  -p 8080:8080 \
-  -v jenkins_home:/var/jenkins_home \
-  -v /var/run/docker.sock:/var/run/docker.sock \
-  jenkins/jenkins:lts-jdk21  
+docker run -d --name jenkins --network devops-net --restart always -p 8080:8080 -v jenkins_home:/var/jenkins_home -v /var/run/docker.sock:/var/run/docker.sock jenkins/jenkins:lts-jdk21  
   
 echo "==> Levantando SonarQube..."  
-docker run -d \
-  --name sonarqube \
-  --network devops-net \
-  --restart always \
-  -p 9000:9000 \
-  -v sonarqube_data:/opt/sonarqube/data \
-  -v sonarqube_logs:/opt/sonarqube/logs \
-  -v sonarqube_extensions:/opt/sonarqube/extensions \
-  sonarqube:community  
+docker run -d --name sonarqube --network devops-net --restart always -p 9000:9000 -v sonarqube_data:/opt/sonarqube/data -v sonarqube_logs:/opt/sonarqube/logs -v sonarqube_extensions:/opt/sonarqube/extensions sonarqube:community  
   
 echo ""  
 echo "Jenkins   -> http://localhost:8080"  
